0cc387a9d4451722ed7daea4e628241bf35fde03
dodikk <dodikk88.reg@gmail.com>
2012-04-12 17:56:19 +0300

    Added schema constants file

---
+++ CsvToSqlite/SqliteTypes.m
@@ -0,0 +1,17 @@
+#import "SqliteTypes.h"
+
+
+NSString* const SQL_NONE     = @"NONE"    ;
+NSString* const SQL_BLOB     = @"BLOB"    ;
+NSString* const SQL_BOOLEAN  = @"BOOLEAN" ;
+NSString* const SQL_CHAR     = @"CHAR"    ;
+NSString* const SQL_DATE     = @"DATE"    ;
+NSString* const SQL_DATETIME = @"DATETIME";
+NSString* const SQL_INT      = @"INT"     ;
+NSString* const SQL_INTEGER  = @"INTEGER" ;
+NSString* const SQL_NUMERIC  = @"NUMERIC" ;
+NSString* const SQL_REAL     = @"REAL"    ;
+NSString* const SQL_TEXT     = @"TEXT"    ;
+NSString* const SQL_VARCHAR  = @"VARCHAR" ;
+
+
