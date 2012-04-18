cb5dc851115d62515068aa44e230ec5a4cbb6184
dodikk <dodikk88.reg@gmail.com>
2012-04-18 11:10:29 +0300

    Added primary key query test

--- CsvToSqliteTests/DbImportTest.mm
+++ CsvToSqliteTests/DbImportTest.mm
@@ -253,6 +253,82 @@
    NSLog( @"%@", [ qLog_ objectAtIndex: 4 ] );   
 }
 
+-(void)testPrimaryKeyQueries
+{
+   NSError*  error_    = nil;
+   NSString* query_    = nil;
+   NSRange substringRange_ = { 0u, 0u };
+   NSRange emptyRange_ =  { 0u, 0u };
+   
+   
+   NSString* csvPath_ = [ [ NSBundle bundleForClass: [ self class ] ] pathForResource: @"Campaigns-small-win" 
+                                                                               ofType: @"csv" ];
+   
+   CsvToSqlite* converter_ = [ [ CsvToSqlite alloc ] initWithDatabaseName: @"3.sqlite" 
+                                                             dataFileName: csvPath_ 
+                                                           databaseSchema: schema_ 
+                                                               primaryKey: primaryKey_
+                                                            separatorChar: ';'
+                                                               lineReader: [ WindowsLineReader new ] 
+                                                           dbWrapperClass: [ MockDb class ] ];
+   
+   MockDb* dbWrapper_ = ( MockDb* )converter_.dbWrapper ;
+   STAssertNotNil( dbWrapper_, @"DB initialization error ");
+   
+   
+   [ converter_  storeDataInTable: @"Campaigns" 
+                            error: &error_ ];   
+   STAssertNil( error_, @"Unexpected error" );
+   
+   NSArray* qLog_ = [ dbWrapper_ queriesLog ]; 
+   STAssertTrue( 5 == [ qLog_ count ], @"Queries count mismatch" );
+   
+   {
+#if 0
+      {
+         expected_ = @"CREATE TABLE [Campaigns]"
+         @" ( [Date] DATE, [Visits] INTEGER, [Value] INTEGER,"
+         @" [FacetId1] VARCHAR, [FacetId2] VARCHAR, [FacetId3] VARCHAR );" ;
+      }
+#endif
+      
+      query_    = [ qLog_ objectAtIndex: 0 ];
+      
+      NSString* prefix_ = @"CREATE TABLE [Campaigns] ( ";
+      BOOL prefixOk_ = [ query_ hasPrefix: prefix_ ];
+      substringRange_ = [ query_ rangeOfString: prefix_ ];
+      
+      
+      STAssertTrue( prefixOk_, @"CREATE TABLE bad start" );
+      STAssertTrue( [ query_ hasSuffix: @", CONSTRAINT pkey PRIMARY KEY ( Date, FacetId1, FacetId2, FacetId3 ) );" ], @"CREATE TABLE bad end" );
+      
+      substringRange_ = [ query_ rangeOfString: @"[Date] DATE" ];
+      STAssertFalse( NSEqualRanges( substringRange_, emptyRange_ ) , @"date missing" );
+      
+      substringRange_ = [ query_ rangeOfString: @"[Visits] INTEGER" ];
+      STAssertFalse( NSEqualRanges( substringRange_, emptyRange_ ) , @"date missing" );
+      
+      substringRange_ = [ query_ rangeOfString: @"[Value] INTEGER" ];
+      STAssertFalse( NSEqualRanges( substringRange_, emptyRange_ ) , @"date missing" );
+      
+      substringRange_ = [ query_ rangeOfString: @"[FacetId1] VARCHAR" ];
+      STAssertFalse( NSEqualRanges( substringRange_, emptyRange_ ) , @"date missing" );
+      
+      substringRange_ = [ query_ rangeOfString: @"[FacetId2] VARCHAR" ];
+      STAssertFalse( NSEqualRanges( substringRange_, emptyRange_ ) , @"date missing" );
+      
+      substringRange_ = [ query_ rangeOfString: @"[FacetId3] VARCHAR" ];
+      STAssertFalse( NSEqualRanges( substringRange_, emptyRange_ ) , @"date missing" );
+   }
+ 
+   
+   NSLog( @"%@", [ qLog_ objectAtIndex: 0 ] );
+   NSLog( @"%@", [ qLog_ objectAtIndex: 1 ] );
+   NSLog( @"%@", [ qLog_ objectAtIndex: 2 ] );   
+   NSLog( @"%@", [ qLog_ objectAtIndex: 3 ] );   
+   NSLog( @"%@", [ qLog_ objectAtIndex: 4 ] );   
+}
+
 -(void)testSameDataImportDoesNotChangeDb
 {
    NSError*  error_    = nil;
