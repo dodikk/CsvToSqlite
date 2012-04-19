faafd0836b7107b120a278b1e238fe5b68be1b5d
dodikk <dodikk88.reg@gmail.com>
2012-04-19 16:35:11 +0300

    Fixed errors in macros

--- CsvToSqlite/CsvMacros.h
+++ CsvToSqlite/CsvMacros.h
@@ -7,23 +7,25 @@
       return nil;            \
    }
 
-#define INIT_ASSERT_EMPTY_STRING( X ) \
-   if ( nil == X || @"" == X )        \
-   {                                  \
-      return nil;                     \
+#define INIT_ASSERT_EMPTY_STRING( X )            \
+   if ( nil == X || [ @"" isEqualToString: X ] ) \
+   {                                             \
+      return nil;                                \
    }
 
 
 #define CHECK_ERROR__RET_BOOL( ERROR_PTR ) \
-   if ( nil != *error_ )                   \
+   if ( nil != *ERROR_PTR )                \
    {                                       \
+      NSLog( @"%@", *ERROR_PTR );          \
       return NO;                           \
    }
 
 
 #define CHECK_ERROR__RET( ERROR_PTR ) \
-   if ( nil != *error_ )              \
+   if ( nil != *ERROR_PTR )           \
    {                                  \
+      NSLog( @"%@", *ERROR_PTR );     \
       return;                         \
    }
 
