7f450e06afd253e8b1946d82bbe90755c1d92ec8
dodikk <dodikk88.reg@gmail.com>
2012-09-12 13:02:09 +0300

    Added DB protocols dependence

--- SetupDependencies.sh
+++ SetupDependencies.sh
@@ -3,3 +3,4 @@ cd ..
 git clone git://github.com/dodikk/ObjcScopedGuard.git
 git clone git clone git://github.com/dodikk/ESLocale.git
 git clone git clone git://github.com/ccgus/fmdb.git
+git clone https://github.com/dodikk/ESDatabaseWrapper.git
