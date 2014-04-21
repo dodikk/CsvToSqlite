dd6213b10b1d2e3be2b440d0eb63f6606a4c37c4
dodikk <dodikk88.reg@gmail.com>
2014-04-21 21:30:40 +0300

    [doc] Added comments for error classes

--- CsvToSqlite/Errors/CsvImportError.h
+++ CsvToSqlite/Errors/CsvImportError.h
@@ -1,16 +1,50 @@
 #import <Foundation/Foundation.h>
 
-enum CsvImportErrorsEnum
+
+/**
+ Error codes for CsvImportError
+ */
+typedef NS_ENUM( NSInteger, CsvImportErrors)
 {
+    /**
+     Import successfull.
+     */
     CSV_NO_ERROR = 0,
+
+    /**
+     Table name string is nil or empty.
+     */
     CSV_SQL_BAD_TABLE_NAME,
+
+    /**
+     Actual CSV schema does not match the expected one for SQL.
+     */
     CSV_SQL_SCHEMA_MISMATCH,
+    
+    /**
+     A parser for CSV columns has not been initialized properly.
+     */
     CSV_PARSER_INITIALIZATION_ERROR,
 };
-typedef NSInteger CsvImportErrors;
 
+
+
+/**
+ A base class for CSV conversion errors :
+ 
+ * CsvBadTableNameError
+ * CsvInitializationError
+ * CsvSchemaMismatchError
+
+ */
 @interface CsvImportError : NSError
 
+
+/**
+ A preferred initializer.
+ 
+ Initializes an NSError object with one of the CsvImportErrors values.
+ */
 -(id)initWithErrorCode:( NSInteger )errorCode_;
 
 @end
