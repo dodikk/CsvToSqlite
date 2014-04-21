d4eca9649278035bdc2b236ba283ca394f663363
dodikk <dodikk88.reg@gmail.com>
2014-04-21 21:38:02 +0300

    [doc] Using only NS_ENUM syntax due to issues with appledoc reference

--- CsvToSqlite/CsvLineEndings.h
+++ CsvToSqlite/CsvLineEndings.h
@@ -1,26 +1,21 @@
 #ifndef CsvToSqlite_CsvLineEndings_h
 #define CsvToSqlite_CsvLineEndings_h
 
-#ifdef __OBJC__
+
     #import <Foundation/Foundation.h>
-#endif
+
 
 /**
  Line ending style enumeration.
  */
-
-#ifdef __OBJC__
 typedef NS_ENUM( NSInteger, CsvLineEndings )
-#else
-enum CsvLineEndingsEnum
-#endif // __OBJC__
 {
     /**
      CR LF - Windows style.
      */
    CSV_LE_WIN        = 0x0D0A,
     
-    /*
+    /**
      LF - Unix and Mac OS X style
      */
    CSV_LE_UNIX       = 0x0A  ,
@@ -31,9 +26,5 @@ enum CsvLineEndingsEnum
      */
    CSV_LE_MAC_LEGACY = 0x0D  ,
 };
-
-#ifndef __OBJC__
-typedef NSInteger CsvLineEndings;
-#endif // __OBJC__
 
 #endif // CsvToSqlite_CsvLineEndings_h
