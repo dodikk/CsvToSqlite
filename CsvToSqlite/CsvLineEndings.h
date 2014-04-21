f5c923b4e65fc6d777fe56b660b255552cc4cc36
dodikk <dodikk88.reg@gmail.com>
2014-04-21 21:04:04 +0300

    [doc] Commented line ending constants

--- CsvToSqlite/CsvLineEndings.h
+++ CsvToSqlite/CsvLineEndings.h
@@ -1,11 +1,25 @@
 #ifndef CsvToSqlite_CsvLineEndings_h
 #define CsvToSqlite_CsvLineEndings_h
 
-
+/**
+ Line ending style enumeration.
+ */
 enum CsvLineEndingsEnum
 {
+    /**
+     CR LF - Windows style.
+     */
    CSV_LE_WIN        = 0x0D0A,
+    
+    /*
+     LF - Unix and Mac OS X style
+     */
    CSV_LE_UNIX       = 0x0A  ,
+    
+    
+    /**
+     CR - Legacy mac style
+     */
    CSV_LE_MAC_LEGACY = 0x0D  ,
 };
 typedef NSInteger CsvLineEndings;
